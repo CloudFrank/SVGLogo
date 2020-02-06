f34ab10c5cc7cf4fb1a618e0d3ac311388f86c42
CloudFrank.com <57855650+CloudFrank@users.noreply.github.com>
2020-02-07 06:07:42 +0800

    Update codepen.js

--- codepen.js
+++ codepen.js
@@ -1,3 +1,18 @@
+import SVGMark from 'https://cloudfrank.github.io/SvgMark/SVGMark.js';
+customElements.define('svg-mark', SVGMark, { extends: "div"});
+import SVGMarks from 'https://cloudfrank.github.io/SvgMark/SVGMarks.js';
+customElements.define('svg-marks', SVGMarks, { extends: "div"});
+
+import CloudfrankLogomark from 'https://cloudfrank.github.io/SvgMark/CloudfrankLogomark.js';
+customElements.define('cloudfrank-logomark', CloudfrankLogomark, { extends: "div"});
+import CloudfrankLogotype from 'https://cloudfrank.github.io/SvgMark/CloudfrankLogotype.js';
+customElements.define('cloudfrank-logotype', CloudfrankLogotype, { extends: "div"});
+
+import WordrinkLogomark from 'https://cloudfrank.github.io/SvgMark/WordrinkLogomark.js';
+customElements.define('wordrink-logomark', WordrinkLogomark, { extends: "div"});
+import WordrinkLogotype from 'https://cloudfrank.github.io/SvgMark/WordrinkLogotype.js';
+customElements.define('wordrink-logotype', WordrinkLogotype, { extends: "div"});
+
 import SVGLogo from 'https://cloudfrank.github.io/SVGLogo/SVGLogo.js';
 customElements.define('svg-logo', SVGLogo, { extends: "div"});
 import SVGLogos from 'https://cloudfrank.github.io/SVGLogo/SVGLogos.js';
